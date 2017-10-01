83dd3600a2c3dca2963f88acbeaec767de20b678
Roderik Acevedo <rode31x@gmail.com>
2017-09-30 21:20:49 -0500

    fs

--- files/A6/A6.php
+++ files/A6/A6.php
@@ -2,18 +2,33 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Asignación N°6</title>
+	<title>Conversión Dec a Bin</title>
 </head>
 <body>
-	 <header><h1>Asignación N°6</h1></header>
+	 <center>
+	 	<h3>Conversión de un decimal a binario</h3>
+	 	<form action="lab4.php" method="post">
+	 		Introducir numero a convertir
+	 		<input type="text" name="num" maxlength="5" size="5">
+	 		<input type="submit" name="envio">
+	 	</form>
+	 	<<?php 
 
-	<<?php 
+	 		if (!empty($_POST["num"])) {
+	 			$NumDecimal=$_POST["num"]
+	 			echo "El numero decimal es: <b>$NumDecimal</b><br>";
+	 			$NumBinario=' ';
+	 			do{
+	 				$NumBinario= $NumDecimal%2 . $NumBinario;
+	 				echo "Resultado intermedio: <b> $NumBinario</b><br>\n";
+	 				$NumDecimal=(int)($NumDecimal/2);
+	 			}while ($NumDecimal>0);
+	 			echo "Numero binario resultante: <b>$NumBinario</b><br>";
+	 			}
+	 		}
 
-
-
-
-
-	 ?>
+	 	 ?>
+	 </center>
 
 </body>
 </html>
