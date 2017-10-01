b9b52682616de675cf75c13fae6910ab872faa46
Roderik A <rode31x@gmail.com>
2017-09-30 19:13:13 -0500

    yup

---
+++ files/A6/A6.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Asignación N°6</title>
+</head>
+<body>
+	 <header><h1>Asignación N°6</h1></header>
+
+	<<?php 
+
+
+
+
+
+	 ?>
+
+</body>
+</html>
